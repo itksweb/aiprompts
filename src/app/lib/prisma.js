322992162bed3e4e08e9ce2f01f204a730ef0cec
itksweb <itksweb@gmail.com>
2023-12-10 22:27:58 +0100

    edited prisma.js file

--- src/app/lib/prisma.js
+++ src/app/lib/prisma.js
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma = new PrismaClient();
+const globalForPrisma = global;
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
