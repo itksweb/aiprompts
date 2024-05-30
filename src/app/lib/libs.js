822d6918e0c91af6e9b3987f4467f1582732f97c
itksweb <66902194+itksweb@users.noreply.github.com>
2024-05-30 11:00:50 +0100

    Update libs.js

--- src/app/lib/libs.js
+++ src/app/lib/libs.js
@@ -2,3 +2,7 @@ export const api =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
     : "https://aiprompts-pi.vercel.app/api";
+export const site_url =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000/"
+    : "https://aiprompts-pi.vercel.app/"
